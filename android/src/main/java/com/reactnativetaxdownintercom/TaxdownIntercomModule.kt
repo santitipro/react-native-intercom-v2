f3aff53dbebd3674ffb1fbaee2b89796f95d44bd
Santi López Bayo <santitipro@hotmail.com>
2020-11-21 14:26:03 +0100

    initial commit

--- android/src/main/java/com/reactnativetaxdownintercom/TaxdownIntercomModule.kt
+++ android/src/main/java/com/reactnativetaxdownintercom/TaxdownIntercomModule.kt
@@ -1,24 +1,66 @@
 package com.reactnativetaxdownintercom
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.nfc.Tag
+import android.util.Log
+import com.facebook.react.bridge.*
+import io.intercom.android.sdk.Intercom
+import io.intercom.android.sdk.identity.Registration
+import java.lang.Error
+import java.lang.Exception
 
 class TaxdownIntercomModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
+    companion object {
+      const val TAG = "Intercom";
+      const val MODULE_NAME = "TaxdownIntercom";
+    }
+
     override fun getName(): String {
-        return "TaxdownIntercom"
+        return MODULE_NAME;
+    }
+
+    @ReactMethod
+    fun identifier(options: ReadableMap, promise: Promise) {
+      try {
+        val userId = options.getString("userId");
+        val registration: Registration = Registration.create().withUserId(userId!!);
+        Intercom.client().registerIdentifiedUser(registration);
+        promise.resolve(null);
+      } catch (e: Exception) {
+        Log.e(TAG, "Failed initialize");
+        promise.resolve(null);
+      }
+    }
+
+    @ReactMethod
+    fun displayMessenger(promise: Promise) {
+      try {
+        Intercom.client().displayMessenger();
+        promise.resolve(null);
+      } catch (e: Exception) {
+        Log.e(TAG, "Failed initialize messenger");
+      }
+    }
+
+    @ReactMethod
+    fun displayHelpCenterArticle(articleId: String, promise: Promise) {
+      try {
+        Intercom.client().displayArticle(articleId);
+        promise.resolve(null);
+      } catch (e: Exception) {
+        promise.reject(e.message);
+        Log.e(TAG, "Failed display article");
+      }
     }
 
     // Example method
     // See https://facebook.github.io/react-native/docs/native-modules-android
     @ReactMethod
     fun multiply(a: Int, b: Int, promise: Promise) {
-    
+
       promise.resolve(a * b)
-    
+
     }
 
-    
+
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { NativeModules } from 'react-native';
 
 type TaxdownIntercomType = {
+  identifier(options: { userId: string }): Promise<void>;
+  displayMessenger(): Promise<void>;
+  displayHelpCenterArticle(articleId: string): Promise<void>;
   multiply(a: number, b: number): Promise<number>;
 };
 
